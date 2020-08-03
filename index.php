e11287a1d00af697d7233c57380764201b3f9197
cpulover <justacpulover@gmail.com>
2020-08-03 23:30:35 +0200

    Basic

--- index.php
+++ index.php
@@ -1,11 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Hello PHP</h1>
-</body>
-</html>
+<?php
+$name = "cpulover";
+echo $name;
+
+echo "<p> I am $name </p>";
+echo " I am " . $name . "<br>"; // concentrate string
+
+$variableName = "name";
+//nested variable
+echo $$variableName . "<br>"; //output = $name = "cpulover"
+
+$myInfo = array("Hieu", "Ngo", "173");
+$myInfo[] = true;
+print_r($myInfo);
+echo $myInfo[5];
+
+$myInfo2 = array(
+    "firstName" => "Hieu",
+    "lastName" => "Ngo",
+);
+$myInfo2["age"] = 23;
+
+echo "<br>";
+print_r($myInfo2);
+echo "<br>Array size: " . sizeof($myInfo2);
+
+echo "<br>";
+foreach ($myInfo2 as $key => $value) {
+    echo $key . ": " . $value . "<br>";
+}
